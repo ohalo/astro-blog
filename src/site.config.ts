637a933eb7d6e28684a012f645166359f496b895
赵辉亮 <zhaohuiliang@bgyfw.com>
2026-05-22 14:14:50 +0800

    config: update waline server URL to custom domain

--- src/site.config.ts
+++ src/site.config.ts
@@ -162,7 +162,7 @@ export const integ: IntegrationUserConfig = {
   waline: {
     enable: true,
     // Server service link
-    server: 'https://astro-theme-pure-waline.arthals.ink/',
+    server: 'https://walinehaloblog.halo26812.eu.org/',
     // Show meta info for comments
     showMeta: false,
     // Refer https://waline.js.org/en/guide/features/emoji.html
